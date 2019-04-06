823345eaed694a20fc5b749346b32aebeaf7b566
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-06 06:16:19 +0000

    Deploy projecto-ipm-2019/igo to github.com/projecto-ipm-2019/igo.git:gh-pages

---
+++ precache-manifest.f3cb43b5dcf7b4c2ed2a04fe93ae6ea9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d7c8d560e0d14f1ba7",
+    "url": "/igo/static/css/main.11dbd1e7.chunk.css"
+  },
+  {
+    "revision": "74d7c8d560e0d14f1ba7",
+    "url": "/igo/static/js/main.293a7314.chunk.js"
+  },
+  {
+    "revision": "3c48dfc41aca70a35e3a",
+    "url": "/igo/static/js/runtime~main.1acb7b02.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/igo/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d5ae1eec6fc037ea1070320516c73324",
+    "url": "/igo/index.html"
+  }
+];
