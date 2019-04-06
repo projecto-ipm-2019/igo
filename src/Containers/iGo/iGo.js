d6130b34a9eb9302a2a8365bba1629abb31d85e5
AndreasBBS <andreas.bbs@gmail.com>
2019-04-06 05:54:37 +0100

    Initialize iGo Project

---
+++ src/Containers/iGo/iGo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./iGo.css"
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="iGo">
+        Teste check mic 1 2 3 going overboard and it all stays inside this box!
+      </div>
+    );
+  }
+}
