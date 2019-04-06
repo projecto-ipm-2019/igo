8fb15a5bbd904f547d604948882031265bb85605
AndreasBBS <andreas.bbs@gmail.com>
2019-04-06 07:09:09 +0100

    Fix Travis CD

--- src/Containers/iGo/iGo.js
+++ src/Containers/iGo/iGo.js
@@ -5,7 +5,9 @@ export default class extends Component {
   render() {
     return (
       <div className="iGo">
-        Teste check mic 1 2 3 going overboard and it all stays inside this box!
+        Test check mic 1 2 3 going overboard and it all stays inside this box!
+        <br/>
+        Travis CD test
       </div>
     );
   }
