3f95acf67882744d937ec242c42c1508857241f8
AndreasBBS <andreas.bbs@gmail.com>
2019-04-23 12:07:23 +0100

    Making a local build for v0.1.0

---
+++ precache-manifest.570bc15d44bcdc2e81afb86d305e5fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f386963770b6df7b7661",
+    "url": "/static/js/main.97e4f5bc.chunk.js"
+  },
+  {
+    "revision": "77ff384904d4a1158cc4",
+    "url": "/static/js/2.97d1851b.chunk.js"
+  },
+  {
+    "revision": "f386963770b6df7b7661",
+    "url": "/static/css/main.ec1b567d.chunk.css"
+  },
+  {
+    "revision": "25e99fe7cfbbbe620708438756b5d734",
+    "url": "/index.html"
+  }
+];
