90eb7ed62ad926b8c04b6533c6f6f6155d727821
AndreasBBS <andreas.bbs@gmail.com>
2019-04-30 04:19:33 +0100

    Local build for version 0.2.0 of the Interface

---
+++ precache-manifest.7715f06745f129e229dc5d2d48cb248b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "08b052acf8fea8d4d2a40a1c6994a714",
+    "url": "./static/media/qr--igo.08b052ac.svg"
+  },
+  {
+    "revision": "d48d37797356fd89c0c3",
+    "url": "./static/css/main.0ebf6e74.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3d5d12f5d2419db27501",
+    "url": "./static/js/2.c0699dc8.chunk.js"
+  },
+  {
+    "revision": "72b7d88c7d39dd058c819fa46a9f2db5",
+    "url": "./static/media/friends.72b7d88c.svg"
+  },
+  {
+    "revision": "e8f6f6d13585dc19174d4409223057c1",
+    "url": "./static/media/events.e8f6f6d1.svg"
+  },
+  {
+    "revision": "d9336abc9e04730b64facba1be452053",
+    "url": "./static/media/newnotifications.d9336abc.png"
+  },
+  {
+    "revision": "d48d37797356fd89c0c3",
+    "url": "./static/js/main.c9803a5d.chunk.js"
+  },
+  {
+    "revision": "cc9a2475e7457ee818d4b2d71e7ab022",
+    "url": "./static/media/trash.cc9a2475.png"
+  },
+  {
+    "revision": "da2a31f5c8df03f83b6d1d4445c74888",
+    "url": "./static/media/iGOlogo.da2a31f5.jpg"
+  },
+  {
+    "revision": "68aa7d9a2cac815df80922861943c259",
+    "url": "./static/media/79718.68aa7d9a.jpg"
+  },
+  {
+    "revision": "1bb81d2b072d4b42a5e58a81df0adb0b",
+    "url": "./static/media/home.1bb81d2b.svg"
+  },
+  {
+    "revision": "0325ab33e06634ad887a61d7a35038eb",
+    "url": "./static/media/return.0325ab33.svg"
+  },
+  {
+    "revision": "8414d4c5a9dcff67a392b68782a6a9f8",
+    "url": "./index.html"
+  }
+];
